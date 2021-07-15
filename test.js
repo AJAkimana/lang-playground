9ef50fc147acd6f86e59fe90caaa65ed0a369705
Akimana J d Amour <akimanaja17@gmail.com>
2021-07-15 13:38:18 +0200

    Test dart model json data

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 console.log('Let start');
-const isAnagram = (s,t) => ( [...s].sort().join('') === [...t].sort().join(''))
+const isAnagram = (s, t) => [...s].sort().join('') === [...t].sort().join('');
 
-console.log( isAnagram('code', 'doce') )  // true
+console.log(isAnagram('code', 'doce')); // true
 // console.log( isAnagram('anagram', 'xxx') )     // false
 // console.log(sum(23));
