d15b875cced8f54738e96ab57392bc1ce36df068
Jean d Amour AKIMANIZANYE <30657705+AJAkimana@users.noreply.github.com>
2025-03-11 17:28:52 +0200

    Write validate the string paranthesis

--- test.js
+++ test.js
@@ -4,3 +4,27 @@ const isAnagram = (s, t) => [...s].sort().join('') === [...t].sort().join('');
 console.log(isAnagram('code', 'doce')); // true
 // console.log( isAnagram('anagram', 'xxx') )     // false
 // console.log(sum(23));
+
+var isValid = function(s) {
+    var stack = []
+    var pairs = {
+        '(':')',
+        '{':'}',
+        '[':']'
+    }
+    for (var c of s){
+        if(pairs[c]){
+            stack.push(c)
+        }else if(c === ')' || c === '}' || c === ']'){
+            if(pairs[stack.pop()] !== c){
+                return false
+            }
+        }
+    }
+    return stack.length === 0
+};
+
+let s = readline();
+
+if(isValid(s)) console.log("valid");
+else console.log("invalid");
